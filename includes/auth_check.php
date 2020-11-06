91de642e91afe90f8b347187cf842f6c685c1627
YanickGit <46899594+YanickGit@users.noreply.github.com>
2020-11-06 12:55:35 -0500

    Update auth_check.php

--- includes/auth_check.php
+++ includes/auth_check.php
@@ -1,5 +1,5 @@
 <?php
-    if ($title <> 'Index' || $title <> 'Success'){
+    if (!$title = 'Index' || !$title = 'Success'){
         if (!isset($_SESSION['user_id'])){
             header('Location: login.php');
         }
