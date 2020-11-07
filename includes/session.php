a5034086796da07e576bde1b1728331b4996bbec
YanickGit <46899594+YanickGit@users.noreply.github.com>
2020-11-07 08:12:52 -0500

    Session Timeout after 5 minutes

--- includes/session.php
+++ includes/session.php
@@ -1,3 +1,16 @@
 <?php
     session_start();
+
+    $time = $_SERVER['REQUEST_TIME'];
+
+    $timeout_duration = 300; //5 minutes timeout
+
+    if (isset($_SESSION['LAST_ACTIVITY']) && 
+        ($time - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
+        session_unset();
+        session_destroy();
+        session_start();
+    }
+
+    $_SESSION['LAST_ACTIVITY'] = $time; //update time based on user activity
 ?>
